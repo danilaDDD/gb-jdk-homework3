972c354db70de3fc89480394109af2d1882214b2
danila nikitin <danila.n2015@yandex.ru>
2024-08-19 20:02:19 +0300

    fix calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,29 +1,17 @@
 public class Calculator {
-    public static <N extends Number> double sum(N number1, N number2){
-        double doubleNumber1 = (double) number1;
-        double doubleNumber2 = (double) number2;
-
-        return doubleNumber1 + doubleNumber2;
+    public static double sum(double number1, double number2){
+        return number1 + number2;
     }
 
-    public static <N extends Number> double multiply(N number1, N number2){
-        double doubleNumber1 = (Double) number1;
-        double doubleNumber2 = (Double) number2;
-
-        return doubleNumber1 * doubleNumber2;
+    public static double multiply(double number1, double number2){
+        return number1 * number2;
     }
 
-    public static <N extends Number> double divide(N number1, N number2){
-        double doubleNumber1 = (Double) number1;
-        double doubleNumber2 = (Double) number2;
-
-        return doubleNumber1 / doubleNumber2;
+    public static double divide(double number1, double number2){
+        return number1 / number2;
     }
 
-    public static <N extends Number> double subtract(N number1, N number2){
-        double doubleNumber1 = (Double) number1;
-        double doubleNumber2 = (Double) number2;
-
-        return doubleNumber1 - doubleNumber2;
+    public static double subtract(double number1, double number2){
+        return number1 - number2;
     }
 }
