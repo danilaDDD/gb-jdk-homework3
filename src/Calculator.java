3539a41f49717952b2426871ff9e1356e93f2132
danila nikitin <danila.n2015@yandex.ru>
2024-08-19 19:54:42 +0300

    calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,29 @@
 public class Calculator {
+    public static <N extends Number> double sum(N number1, N number2){
+        double doubleNumber1 = (double) number1;
+        double doubleNumber2 = (double) number2;
+
+        return doubleNumber1 + doubleNumber2;
+    }
+
+    public static <N extends Number> double multiply(N number1, N number2){
+        double doubleNumber1 = (Double) number1;
+        double doubleNumber2 = (Double) number2;
+
+        return doubleNumber1 * doubleNumber2;
+    }
+
+    public static <N extends Number> double divide(N number1, N number2){
+        double doubleNumber1 = (Double) number1;
+        double doubleNumber2 = (Double) number2;
+
+        return doubleNumber1 / doubleNumber2;
+    }
+
+    public static <N extends Number> double subtract(N number1, N number2){
+        double doubleNumber1 = (Double) number1;
+        double doubleNumber2 = (Double) number2;
+
+        return doubleNumber1 - doubleNumber2;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer i = 1;
+        Integer i2 = 2;
+        System.out.println(Calculator.sum(i, i2));
+        System.out.println(Calculator.divide(i, i2));
     }
 }
